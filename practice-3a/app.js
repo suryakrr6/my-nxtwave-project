d8deb72b14e0698dfee5b7253a806abc3e64510b
surya <suryakrr6@gmail.com>
2026-03-16 10:48:54 +0000

    commit

---
+++ practice-3a/app.js
@@ -0,0 +1,15 @@
+const express = require('express')
+const addDays = require('date-fns/addDays')
+const app = express()
+
+app.get('/', (req, res) => {
+  const dateTime = new Date()
+  const result = addDays(
+    new Date(dateTime.getFullYear(), dateTime.getMonth(), dateTime.getDate()),
+    100,
+  )
+  res.send(
+    `${result.getDate()}/${result.getMonth() + 1}/${result.getFullYear()}`,
+  )
+})
+module.exports = app
