c5b3f3676f54e6f91cbfcae74d2bf06c34b2364d
surya <suryakrr6@gmail.com>
2026-03-16 06:49:18 +0000

    commit

---
+++ practice-3b/app.js
@@ -0,0 +1,9 @@
+const express = require('express')
+const app = express()
+
+app.get('/gadgets', (req, res) => {
+  res.sendFile('./gadgets.html', {root: __dirname})
+})
+app.listen(3000)
+
+module.exports = app
